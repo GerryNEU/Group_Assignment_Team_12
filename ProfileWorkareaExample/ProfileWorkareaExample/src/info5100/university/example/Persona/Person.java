401606cef684d65806873d27a28f1be654cafbea
Zhizhi Li <li.zhizhi@northeastern.edu>
2025-10-24 19:58:29 -0400

    Performance Reporting

--- ProfileWorkareaExample/ProfileWorkareaExample/src/info5100/university/example/Persona/Person.java
+++ ProfileWorkareaExample/ProfileWorkareaExample/src/info5100/university/example/Persona/Person.java
@@ -11,15 +11,15 @@ package info5100.university.example.Persona;
  */
 public class Person {
     
-    String id;
-    String name;
+    private String personId;
+    private String name;
     
     public Person (String id){
         
-        this.id = id;
+        this.personId = id;
     }
     public String getPersonId(){
-        return id;
+        return personId;
     }
 
     public boolean isMatch(String id){
